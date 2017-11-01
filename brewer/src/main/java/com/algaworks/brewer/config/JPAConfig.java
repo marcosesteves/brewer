535262bf5f542051b515f6442a46f34d6807b6f7
emarco <emarco@CINFO-0841.ctex.eb.mil.br>
2017-11-01 15:42:07 -0200

    Alteração configurando o Spring JPA - 01/11/17

---
+++ brewer/src/main/java/com/algaworks/brewer/config/JPAConfig.java
@@ -0,0 +1,32 @@
+package com.algaworks.brewer.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class JPAConfig {
+
+	// Recuperando o Data Source definido em context.xml
+	@Bean
+	public DataSource dataSource() {
+		JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
+		dataSourceLookup.setResourceRef(true);
+		return dataSourceLookup.getDataSource("jdbc/brewerDB");
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(false);
+		adapter.setGenerateDdl(false);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		return adapter;
+}
+}
